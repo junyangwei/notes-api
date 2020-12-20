997912a697699fcaca33a8871b33f614338abd1c
junyang <weijunyang@guorou.tv>
2020-12-20 22:09:00 +0800

    feat: 修改Django时区配置

--- note_api/settings.py
+++ note_api/settings.py
@@ -123,15 +123,20 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# 修改默认使用的中文
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+# 修改默认时间为亚洲/上海
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
